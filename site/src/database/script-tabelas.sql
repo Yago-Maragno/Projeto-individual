1207bd505d92c86d119dd97ee7dd3c90f2747b8a
Yago-Maragno <yago.maragno@sptech.school>
2022-11-17 14:57:58 -0300

    Site com API

---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,36 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+create table usuario (
+	idPessoa int primary key auto_increment,
+    nome varchar(45),
+    sobreNome varchar(45),
+    email varchar(45),
+    cpf char(11),
+    rg char(9),
+    logradouro varchar(80),
+    bairro varchar(45),
+    numero int,
+    complemento varchar(45),
+    senha varchar(45)
+);
+
+/*
+comandos para criar usuário em banco de dados azure, sqlserver,
+com permissão de insert + update + delete + select
+*/
+
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
+
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
